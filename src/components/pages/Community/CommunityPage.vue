b9269d834bc6e635e1d09a50bad6ac1c1f4f3b86
kkoma2623 <zeghirar@naver.com>
2020-02-22 17:04:29 +0900

    ..

--- src/components/pages/Community/CommunityPage.vue
+++ src/components/pages/Community/CommunityPage.vue
@@ -1,38 +1,175 @@
 <template>
-  <v-tabs
-    active-class='indigo lighten-1'
-    color="pink lighten-4"
-    show-arrows
-  >
-    <v-tabs-slider color="yellow"></v-tabs-slider>
-
-    <v-tab
-      v-for="i in 5"
-      :key="i"
-      :href="'#tab-' + i"
+  <div>
+    <v-toolbar
+      color="light-blue accent-4"
+      dark
+      tabs
     >
-      Item {{ i }}
-    </v-tab>
+      <v-toolbar-title class="white--text">Community</v-toolbar-title>
+
+      <template v-slot:extension>
+        <v-tabs
+          color="light-blue accent-4"
+          grow
+        >
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab
+            v-for="board in boards"
+            :key="board"
+          >
+            {{ board }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
 
     <v-tabs-items>
-      <v-tab-item
-        v-for="i in 30"
-        :key="i"
-        :value="'tab-' + i"
-      >
-        <v-card flat>
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
-      </v-tab-item>
+        <v-tab-item
+            v-for="board in boards"
+            :key="board"
+        >
+            <div>
+                <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    :search="search"
+                    hide-actions
+                    :pagination.sync="pagination"
+                    class="elevation-1"
+                    >
+                    <template v-slot:items="props">
+                        <td>{{ props.item.name }}</td>
+                        <td class="text-xs-right">{{ props.item.calories }}</td>
+                        <td class="text-xs-right">{{ props.item.fat }}</td>
+                        <td class="text-xs-right">{{ props.item.carbs }}</td>
+                        <td class="text-xs-right">{{ props.item.protein }}</td>
+                        <td class="text-xs-right">{{ props.item.iron }}</td>
+                    </template>
+                </v-data-table>
+                <div class="text-xs-center pt-2">
+                    <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+                </div>
+            </div>
+        </v-tab-item>
     </v-tabs-items>
-  </v-tabs>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        tab: null,
+        boards: ['전체', '공지사항', '문제 요청', '규칙 요청', '질문 게시판'],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        search: '',
+        pagination: {},
+        selected: [],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
+            value: 'name'
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' }
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%'
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%'
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%'
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%'
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%'
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%'
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%'
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%'
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%'
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%'
+          }
+        ]
+      }
+    },
+    computed: {
+      pages () {
+        if (this.pagination.rowsPerPage == null ||
+          this.pagination.totalItems == null
+        ) return 0
+
+        return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
       }
     }
   }
