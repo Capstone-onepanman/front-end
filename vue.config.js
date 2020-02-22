2b1b2bbbdc10c9f2243bf405c037afb18ff45484
kkoma2623 <zeghirar@naver.com>
2020-02-22 23:11:28 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
   devServer: {
     disableHostCheck: true
   },
   transpileDependencies: [
     'Vuetify',
-  ]
+  ],
+  chainWebpack: config => {
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag, path, component }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
+  }
 }
